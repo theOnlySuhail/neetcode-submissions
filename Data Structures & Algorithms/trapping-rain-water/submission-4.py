a6fa22d3d4d763c9c2979c1effec3a2c8a6e1a85
suhail <suhail.kamal256@gmail.com>
2026-09-15 01:57:31 +0300

    Bulk sync: 14 submissions

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        map_left = {}
+        map_right = {} 
+        
+        max_left = 0
+        max_right = 0
+
+        # calc max left for each index
+        for i, h in enumerate(height): 
+            map_left[i] = max_left
+            
+            if h >= max_left:
+                max_left = h
+
+        # calc max right for each index
+        for i in range(len(height)): 
+            idx = (len(height) - 1) - i
+            map_right[idx] = max_right
+
+            if height[idx] >= max_right:
+                max_right = height[idx]
+
+        ans = 0
+        for i, h in enumerate(height):
+            if min(map_left[i], map_right[i]) > h:
+                ans += min(map_left[i], map_right[i]) - h
+
+        return ans
+
+        
